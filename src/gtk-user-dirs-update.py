95c0f5561a883a3099c21a18e5b57016b15160de
Ikem Krueger <ikem.krueger@gmail.com>
2022-05-25 18:17:43 +0200

    Generate entries from code

--- src/gtk-user-dirs-update.py
+++ src/gtk-user-dirs-update.py
@@ -6,98 +6,95 @@ from gi.repository import Gtk
 import subprocess
 
 class GtkUserDir(object):
-	def __init__(self):
-		builder = Gtk.Builder()
-		builder.add_from_file("gtk-user-dirs-update.ui")
+    def __init__(self):
+        builder = Gtk.Builder()
+        builder.add_from_file("gtk-user-dirs-update.ui")
 
-		self.window = builder.get_object("dialog1")
-		self.window.set_title("Update User Directories")
-		self.window.connect("destroy", self.quit)
-		self.window.show()
+        self.window = builder.get_object("dialog1")
+        self.window.set_title("Update User Directories")
+        self.window.connect("destroy", self.quit)
 
-		self.entry_desktop = builder.get_object("entry_desktop")
-		self.entry_desktop.connect("button-press-event", self.select_button_xdg_dir, self.entry_desktop, "DESKTOP")
+        self.vbox1 = builder.get_object("vbox1")
 
-		self.entry_documents = builder.get_object("entry_documents")
-		self.entry_documents.connect("button-press-event", self.select_button_xdg_dir, self.entry_documents, "DOCUMENTS")
+        icons = {
+            "DESKTOP":"user-desktop",
+            "DOCUMENTS":"folder-documents",
+            "DOWNLOAD":"folder-download",
+            "MUSIC":"folder-music",
+            "PICTURES":"folder-pictures",
+            "PUBLICSHARE":"folder-publicshare",
+            "TEMPLATES":"folder-templates",
+            "VIDEOS":"folder-videos"
+        }
 
-		self.entry_download = builder.get_object("entry_download")
-		self.entry_download.connect("button-press-event", self.select_button_xdg_dir, self.entry_download, "DOWNLOAD")
+        for name in sorted(icons.keys()):
+            image = Gtk.Image()
+            image.set_from_icon_name(icons[name], Gtk.IconSize.LARGE_TOOLBAR)
 
-		self.entry_music = builder.get_object("entry_music")
-		self.entry_music.connect("button-press-event", self.select_button_xdg_dir, self.entry_music, "MUSIC")
+            label = Gtk.Label()
+            label.set_label(name.capitalize() + ":")
 
-		self.entry_pictures = builder.get_object("entry_pictures")
-		self.entry_pictures.connect("button-press-event", self.select_button_xdg_dir, self.entry_pictures, "PICTURES")
+            entry = Gtk.Entry()
+            entry.set_text(self.find_xdg_dir(name))
+            entry.connect("button-press-event", self.select_button_xdg_dir, entry, name)
 
-		self.entry_publicshare = builder.get_object("entry_publicshare")
-		self.entry_publicshare.connect("button-press-event", self.select_button_xdg_dir, self.entry_publicshare, "PUBLICSHARE")
+            hbox = Gtk.Box()
 
-		self.entry_templates = builder.get_object("entry_templates")
-		self.entry_templates.connect("button-press-event", self.select_button_xdg_dir, self.entry_templates, "TEMPLATES")
+            hbox.pack_start(image, False, True, 2)
+            hbox.pack_start(label, False, True, 0)
+            hbox.pack_start(entry, True, True, 6)
 
-		self.entry_videos = builder.get_object("entry_videos")
-		self.entry_videos.connect("button-press-event", self.select_button_xdg_dir, self.entry_videos, "VIDEOS")
+            self.vbox1.pack_start(hbox, True, True, 0)
 
-		self.button_reset = builder.get_object("button_reset")
-		self.button_reset.connect("clicked", self.reset_xdg_dirs)
+        self.button_reset = builder.get_object("button_reset")
+        self.button_reset.connect("clicked", self.reset_xdg_dirs)
 
-		#self.button_apply = builder.get_object("button_apply")
-		#self.button_apply.connect("clicked", self.apply)
+        #self.button_apply = builder.get_object("button_apply")
+        #self.button_apply.connect("clicked", self.apply)
 
-		self.update_entries()
+        self.window.show_all()
 
-	def update_entries(self):
-		self.entry_desktop.set_text(self.find_xdg_dir("DESKTOP"))
-		self.entry_documents.set_text(self.find_xdg_dir("DOCUMENTS"))
-		self.entry_download.set_text(self.find_xdg_dir("DOWNLOAD"))
-		self.entry_music.set_text(self.find_xdg_dir("MUSIC"))
-		self.entry_pictures.set_text(self.find_xdg_dir("PICTURES"))
-		self.entry_publicshare.set_text(self.find_xdg_dir("PUBLICSHARE"))
-		self.entry_templates.set_text(self.find_xdg_dir("TEMPLATES"))
-		self.entry_videos.set_text(self.find_xdg_dir("VIDEOS"))
+    def find_xdg_dir(self, name):
+        return subprocess.check_output(['xdg-user-dir', name]).decode().rstrip('\n')
 
-	def find_xdg_dir(self, name):
-		return subprocess.check_output(['xdg-user-dir', name]).decode().rstrip('\n')
+    def set_xdg_dir(self, name, path):
+        # there need to be a check here..
+        subprocess.Popen(['xdg-user-dirs-update', '--set', name, path])
 
-	def set_xdg_dir(self, name, path):
-		# there need to be a check here..
-		subprocess.Popen(['xdg-user-dirs-update', '--set', name, path])
+    def select_entry_xdg_dir(self, widget, entry, name):
+        path = entry.get_text()
 
-	def select_entry_xdg_dir(self, widget, entry, name):
-		path = entry.get_text()
+        self.set_xdg_dir(name, path)
 
-		self.set_xdg_dir(name, path)
+    def select_button_xdg_dir(self, widget, other, entry, name):
+        dialog = Gtk.FileChooserDialog("Select a directory", None,
+                                        Gtk.FileChooserAction.SELECT_FOLDER, 
+                                        (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, 
+                                        Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
 
-	def select_button_xdg_dir(self, widget, other, entry, name):
-		dialog = Gtk.FileChooserDialog("Select a directory", None,
-										Gtk.FileChooserAction.SELECT_FOLDER, 
-										(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, 
-										Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+        dialog.set_current_folder(entry.get_text())
 
-		dialog.set_current_folder(entry.get_text())
+        response = dialog.run()
 
-		response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            path = dialog.get_filename()
 
-		if response == Gtk.ResponseType.OK:
-			path = dialog.get_filename()
+            self.set_xdg_dir(name, path)
 
-			self.set_xdg_dir(name, path)
+            # update_entry
+            entry.set_text(self.find_xdg_dir(name))
 
-			# update_entry
-			entry.set_text(self.find_xdg_dir(name))
+        dialog.destroy()
 
-		dialog.destroy()
+    def reset_xdg_dirs(self, widget):
+        subprocess.Popen(['xdg-user-dirs-update', '--force'])
 
-	def reset_xdg_dirs(self, widget):
-		subprocess.Popen(['xdg-user-dirs-update', '--force'])
+        self.update_entries()
 
-		self.update_entries()
-
-	def quit(self, *args):
-		Gtk.main_quit()
+    def quit(self, *args):
+        Gtk.main_quit()
 
 if __name__ == "__main__":
-	app = GtkUserDir()
-	Gtk.main()
+    app = GtkUserDir()
+    Gtk.main()
 
